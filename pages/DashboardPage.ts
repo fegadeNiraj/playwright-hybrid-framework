0da84f42110e7a179584f555e6ece4e2aa5a4d45
Niraj <niraj@Nirajs-MacBook-Pro.local>
2026-04-10 23:54:23 +0530

    Removed addToCart testcases

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -4,13 +4,9 @@ import { title } from "node:process";
 export class DashboardPage {
     page : Page;
     products: Locator;
-    cart:Locator;
-    cartProducts:Locator;
     constructor(page:Page) {
         this.page = page;
         this.products = page.locator(".card-body");
-        this.cart = page.locator("//button[@routerlink='/dashboard/cart']");
-        this.cartProducts = page.locator(".items");
     }
 
     async waitForDashboardPageToBeLoaded()
@@ -28,71 +24,4 @@ export class DashboardPage {
         return this.products.count();
     }
 
-    async addProductToCart(productName: string) 
-    {
-        const count = await this.products.count();
-        let found = false;
-
-        const cartButton = this.page.locator("//button[@routerlink='/dashboard/cart']");
-
-        const cartTextBefore = await cartButton.textContent();
-        const currentCount = Number(cartTextBefore?.match(/\d+/)?.[0] || 0);
-
-        for (let i = 0; i < count; i++) {
-            const product = this.products.nth(i);
-            const title = await product.locator('h5').textContent();
-
-            if (title?.trim() === productName) {
-
-                await product.getByText('Add To Cart').click();
-
-                await expect(cartButton).toContainText(String(currentCount + 1));
-
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            throw new Error(`Product not found: ${productName}`);
-        }
-    }
-
-    async verifyProductAddedToCart(productName:string)
-    {
-        await this.cart.click();
-        let count = await this.cartProducts.count();
-        let found = false;
-        for(let i=0; i < count; i++)
-        {
-            const product = this.cartProducts.nth(i);
-            const title = await product.locator('h3').textContent();
-
-            if(title?.trim() === productName){
-                found = true;
-                break;
-            }
-        }
-        if(found === false)
-        {
-            throw new Error("Product not found in cart: "+productName);
-        }
-    }
-
-    async verifyMultipleProductsAddedToCart(productsList: string[]) 
-    {
-        await this.cart.click();
-
-        await expect(this.cartProducts.first()).toBeVisible();
-
-        const cartTitles = await this.cartProducts.locator('h3').allTextContents();
-
-        const normalizeCartItems = cartTitles.map(title => title.trim());
-
-        for (const expectedProduct of productsList) {
-            if (!normalizeCartItems.includes(expectedProduct)) {
-                throw new Error(`Product not found in cart: ${expectedProduct}`);
-            }
-        }
-    }
 }
